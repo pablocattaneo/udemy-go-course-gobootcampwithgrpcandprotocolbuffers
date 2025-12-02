7766a1102e089cbd61d7145364d74773a6dec668
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-12-02 10:43:56 -0300

    initial commit

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var age int
+	var age2 = "30"
+	const number = 100
+	const p float64 = 10 / 2
+	fmt.Println(age)
+	fmt.Println(age2)
+	fmt.Println(p)
+	const maxFloat = math.MaxFloat64
+	fmt.Println(maxFloat)
+}
