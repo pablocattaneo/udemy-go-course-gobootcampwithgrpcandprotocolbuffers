2fa76814e3dc17178496c5119a6da1f7fe705104
Pablo Cattaneo <pablocattaneo1@gmail.com>
2025-12-06 19:16:08 -0300

    Add switch.go to demonstrate switch statements for day and number classification

---
+++ basics/switch.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	day := 3
+
+	switch day {
+	case 1:
+		println("Monday")
+	case 2:
+		println("Tuesday")
+	case 3:
+		println("Wednesday")
+	case 4:
+		println("Thursday")
+	case 5:
+		println("Friday")
+	case 6:
+		println("Saturday")
+	case 7:
+		println("Sunday")
+	default:
+		println("Invalid day")
+	}
+
+	number := 2
+
+	switch number {
+	case 1, 3, 5, 7, 9:
+		println("Odd number")
+	case 2, 4, 6, 8, 10:
+		println("Even number")
+	default:
+		println("Number is out of range")
+	}
+}
